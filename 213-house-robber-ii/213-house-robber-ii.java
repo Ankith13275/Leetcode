eb81af8eb01ae9f1b904afc5301c2e58c84f8815
Ankith Lagupudi <56472421+Ankith13275@users.noreply.github.com>
2022-06-10 11:20:54 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (11.64%) - LeetHub

---
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    
+    public int rob(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        int[] nums1 = Arrays.copyOfRange(nums, 1, nums.length);
+        int[] nums2 = Arrays.copyOfRange(nums, 0, nums.length - 1);
+        return Math.max(rob1(nums1), rob1(nums2));
+    }
+    
+    int rob1(int[] nums) {
+        int n = nums.length;
+        int prev = nums[0];
+        int prev2 = 0;  
+        for(int i = 1; i < nums.length; i++){
+            int pick = (i>1) ? prev2 + nums[i] : nums[i];
+            int nopick = prev;
+            int curr = Math.max(pick, nopick);
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    } 
+}
+
+    
