1f3f6b13e41cf5175403029544d014b429bd655e
Ankith Lagupudi <56472421+Ankith13275@users.noreply.github.com>
2022-06-14 23:52:47 +0530

    Time: 10 ms (13.43%), Space: 47.3 MB (57.05%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.java
@@ -2,21 +2,32 @@ class Solution {
     public int minFallingPathSum(int[][] matrix) {
         int n = matrix.length;
         int[][] dp = new int[n][n];
+        
+        for(int i = 0; i < n; i++){
+            dp[0][i] = matrix[0][i];
+        }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j < n; j++){
+                
+                int up = matrix[i][j] + dp[i - 1][j];
+                
+                int ld = matrix[i][j];
+                if (j - 1 >= 0) ld += dp[i - 1][j - 1];
+                else ld += (int)Math.pow(10,9);
+                
+                int rd = matrix[i][j];
+                if(j + 1 < n) rd += dp[i - 1][j + 1];
+                else rd +=(int)Math.pow(10,9);
+                
+                dp[i][j] = Math.min(up, Math.min(ld, rd));
+            }
+        }
+        
         int min = (int)Math.pow(10,9);
         for(int i = 0; i < n; i++){
-            min = Math.min(min, min(matrix, n - 1, i, dp));
+            min = Math.min(min, dp[n - 1][i]);
         }
         return min;
-    }
-    
-    int min(int[][] matrix, int i, int j, int[][] dp){
-        int n = matrix.length;
-        if(j < 0 || j >= n) return (int)Math.pow(10,9);
-        if(i == 0) return matrix[0][j];
-        if(dp[i][j] != 0) return dp[i][j];
-        int up = matrix[i][j] + min(matrix, i - 1, j, dp);
-        int ld = matrix[i][j] + min(matrix, i - 1, j - 1, dp);
-        int rd = matrix[i][j] + min(matrix, i - 1, j + 1, dp);
-        return dp[i][j] = Math.min(up, Math.min(ld, rd));
     }
 }
