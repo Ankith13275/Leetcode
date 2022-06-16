c25fb27bd5fcf79a857a1bf557af7922bcb5b78a
Ankith Lagupudi <56472421+Ankith13275@users.noreply.github.com>
2022-06-16 11:28:06 +0530

    Time: 82 ms (34.50%), Space: 53.9 MB (54.26%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,38 +1,35 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int N = nums.length;
+        int n = nums.length;
         int totalsum = 0;
-        for(int i = 0; i < N; i++){
+        for(int i = 0; i < n; i++){
             totalsum += nums[i];
         }
         if(totalsum % 2 == 1) return false;
         int sum = totalsum / 2;
-        int[][] dp = new int[N][sum + 1];
-        for(int[] ele : dp){
-            Arrays.fill(ele, -1);
-        }
-        return f(N - 1, nums, sum, dp);
+        return f(n, nums, sum);
     }
     
-    boolean f(int N, int arr[], int sum, int[][] dp){
-        if(sum == 0) return true;
-        if(N == 0) return arr[0] == sum;
-        if(dp[N][sum] != -1){
-            if(dp[N][sum] == 1) return true;
-            else if(dp[N][sum] == 0)return false;
+   boolean f(int n, int arr[], int k){
+        boolean dp[][]= new boolean[n][k+1];
+        
+        for(int i=0; i<n; i++){
+            dp[i][0] = true;
+            
         }
         
-        boolean take = false;
-        if(sum >= arr[N]){
-            take = f(N - 1, arr, sum - arr[N], dp);
+        if(arr[0]<=k) dp[0][arr[0]] = true;
+        
+        for(int ind = 1; ind<n; ind++){
+            for(int target= 1; target<=k; target++){
+                boolean notTaken = dp[ind-1][target];
+                boolean taken = false;
+                if(arr[ind]<=target) taken = dp[ind-1][target-arr[ind]];
+                dp[ind][target]= notTaken||taken;
+                
+            }
+            
         }
-        boolean Notake = f(N - 1, arr, sum, dp);
-        
-        boolean ans = take || Notake;
-        
-        if(ans) dp[N][sum] = 1;
-        else dp[N][sum] = 0;
-        
-        return ans;
-    }
+       return dp[n-1][k];
+   }
 }
