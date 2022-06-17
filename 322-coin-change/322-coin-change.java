d588832a7b1c3fdb866c36b7a8ae95ba324a9bb3
Ankith Lagupudi <56472421+Ankith13275@users.noreply.github.com>
2022-06-17 15:32:18 +0530

    Time: 91 ms (12.07%), Space: 55.1 MB (18.31%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,31 +1,28 @@
 class Solution {
-    public int coinChange(int[] arr, int T) {
-         int n= arr.length;
-    
-    int prev[]=new int[T+1]; 
-    int cur[] =new int[T+1];
-    
-    for(int i=0; i<=T; i++){
-        if(i%arr[0] == 0)  
-            prev[i] = i/arr[0];
-        else prev[i] = (int)Math.pow(10,9);
-    }
-    
-    for(int ind = 1; ind<n; ind++){
-        for(int target = 0; target<=T; target++){
-            
-            int notTake = 0 + prev[target];
-            int take = (int)Math.pow(10,9);
-            if(arr[ind]<=target)
-                take = 1 + cur[target - arr[ind]];
-                
-             cur[target] = Math.min(notTake, take);
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[][] dp = new int[n][amount + 1];
+        for(int[] ele : dp){
+            Arrays.fill(ele, -1);
         }
-        prev = cur;
+        int ans = f(n - 1, coins, amount, dp);
+        if(ans > 100000000){
+            return -1;
+        }
+        else return ans;
     }
     
-    int ans = prev[T];
-    if(ans >=(int)Math.pow(10,9)) return -1;
-    return ans;
+    int f(int ind, int[] coins, int amount, int[][] dp){
+        if(ind == 0){
+            if(amount % coins[ind] == 0) return amount / coins[ind];
+            else return (int)(Math.pow(10, 9));
+        }
+        if(dp[ind][amount] != -1) return dp[ind][amount];
+        int notake = 0 + f(ind - 1, coins, amount, dp);
+        int take = Integer.MAX_VALUE;
+        if(coins[ind] <= amount){
+            take = 1 + f(ind, coins, amount - coins[ind], dp);
+        }
+        return dp[ind][amount] = Math.min(take, notake);
     }
 }
